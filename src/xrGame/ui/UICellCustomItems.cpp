11a93da72bfc02dd0ffcd1648bc1822d262c75b8
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-15 00:34:44 +0300

    Fix `xrGameSOC`, part 3

--- src/xrGame/ui/UICellCustomItems.cpp
+++ src/xrGame/ui/UICellCustomItems.cpp
@@ -193,7 +193,7 @@ CUIWeaponCellItem::CUIWeaponCellItem(CWeapon* itm)
 	}
 }
 
-#include "../xrServerEntities/object_broker.h"
+#include "../xrCore/object_broker.h"
 CUIWeaponCellItem::~CUIWeaponCellItem()
 {
 }
