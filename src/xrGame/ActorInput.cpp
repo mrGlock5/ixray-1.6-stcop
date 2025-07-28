2d22d8d219846c832062bd8cbc5f0b49c97c0fb5
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-28 14:49:19 +0300

    Add individual slot for pistols

--- src/xrGame/ActorInput.cpp
+++ src/xrGame/ActorInput.cpp
@@ -918,6 +918,7 @@ static	u16 SlotsToCheck [] = {
 		INV_SLOT_3		,		// 2
 		GRENADE_SLOT	,		// 3
 		ARTEFACT_SLOT	,		// 10
+		PISTOL_SLOT_NEW ,		// 12
 };
 
 void	CActor::OnNextWeaponSlot()
@@ -947,6 +948,10 @@ void	CActor::OnNextWeaponSlot()
 			if (SlotsToCheck[i] == ARTEFACT_SLOT) 
 			{
 				IR_OnKeyboardPress(kARTEFACT);
+			}
+			else if (SlotsToCheck[i] == PISTOL_SLOT_NEW)
+			{
+				IR_OnKeyboardPress(kWPN_7);
 			}
 			else
 				IR_OnKeyboardPress(kWPN_1 + i);
