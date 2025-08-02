0cae2c626e3ff9150b7806bc445dacabd6788abb
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-08-02 23:31:46 +0300

    Replace `CUITextWnd` with `CUIStatic`

--- src/xrUI/UIHelper.h
+++ src/xrUI/UIHelper.h
@@ -9,7 +9,6 @@
 class CUIXml;
 class CUIWindow;
 class CUIStatic;
-class CUITextWnd;
 class CUIProgressBar;
 class CUIProgressShape;
 class CUIFrameLineWnd;
@@ -32,7 +31,6 @@ public:
 
 	static	CUIStatic*			CreateStatic		( CUIXml& xml, LPCSTR ui_path, CUIWindow* parent );
 	static	CUIStackPanel*		CreateStackPanel	( CUIXml& xml, LPCSTR ui_path, CUIWindow* parent );
-	static	CUITextWnd*			CreateTextWnd		( CUIXml& xml, LPCSTR ui_path, CUIWindow* parent );
 	static	CUIProgressBar*		CreateProgressBar	( CUIXml& xml, LPCSTR ui_path, CUIWindow* parent );
 	static	CUIProgressShape*	CreateProgressShape	( CUIXml& xml, LPCSTR ui_path, CUIWindow* parent );
 	static	CUIFrameLineWnd*	CreateFrameLine		( CUIXml& xml, LPCSTR ui_path, CUIWindow* parent, bool critical = true );
