06129e977c72042f5e188024fee36b4d80ebe0a2
mnelenpridumivat <ragrind@mail.ru>
2025-09-22 01:18:23 +0300

    Reapply "Add velocity rotator animator"
    This reverts commit 32714f29b1f0cdf3c48e8c10b6c8a5f4249d81c8.

--- src/xrParticles/psystem.h
+++ src/xrParticles/psystem.h
@@ -66,6 +66,7 @@ namespace PAPI{
 		pVector		vel;     	
 		pVector		velI;
 		pVector		rot_vel;
+		pVector		rot_velS;
 		pVector		size;   
 		pVector		sizeI;  
 		Rotation	rot;	
@@ -141,6 +142,7 @@ namespace PAPI{
 		PAColorAnimatorID,
 		PASizeAnimatorID,
 		PAVelocityAnimatorID,
+		PAVelocityRotationAnimatorID,
 		action_enum_force_dword = u32(-1)
 	};
     struct ParticleAction;
