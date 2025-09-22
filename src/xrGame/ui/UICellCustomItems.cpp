4d6db91c6f67d430eef82c5d904a369215538bd4
wh1t3lord <git.matvey@yandex.ru>
2025-09-22 20:51:09 +0300

    ADD: fix comment; fallback to raster if svg wasn't found;

--- src/xrGame/ui/UICellCustomItems.cpp
+++ src/xrGame/ui/UICellCustomItems.cpp
@@ -30,6 +30,15 @@ CUIInventoryCellItem::CUIInventoryCellItem(CInventoryItem* itm)
 	const char* icons_texture = READ_IF_EXISTS(pSettings, r_string, itm->m_section_id, "icons_texture", nullptr);
 
 	bool isRaster = EngineExternal().isRenderingUIRaster();
+
+	if (!isRaster)
+	{
+		if (EngineExternal().isRenderingUIErrorFallbackToDefaultAtlas()==false)
+		{
+			isRaster = !(pSettings->line_exist(itm->m_section_id, kUIConfigField_InventoryVectorIcon));
+		}
+	}
+
 	m_grid_size.set(itm->GetInvGridRect().rb);
 	if (isRaster)
 	{
